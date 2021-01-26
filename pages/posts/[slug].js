be43d7272e72e3c499e3425df009f9893a45799c
Jenny Ly <jennyly294@gmail.com>
2021-01-26 10:08:08 -0500

    add static paths for dynamic routes

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -20,29 +20,43 @@ function PostTemplate({ content, data }) {
 	);
 };
 
-PostTemplate.getStaticProps = async(context) => {
-	const { slug } = context.query;
+export async function getStaticProps(context) {
+	const { slug } = context.params;
 
 	const content = await import(`../../content/${slug}.md`);
 
-	const data = matter(content.default);
+	const meta = matter(content.default);
 
-	return { ...data };
-
-	return { slug };
+	return {
+		props: {
+			content: meta.content,
+			data: meta.data,
+		}
+	}
 }
 
-PostTemplate.getStaticPaths = async() => {
-	const fs = require("fs");
-	const files = fs.readdirSync(`${process.cwd()}/content`, "utf-8");
+export async function getStaticPaths() {
+	const context = require.context('../../content', false, /\.md$/)
+  const posts = []
+  for(const key of context.keys()){
+    const post = key.slice(2);
+    const content = await import(`../../content/${post}`);
+    const meta = matter(content.default)
+    posts.push({
+      slug: post.replace('.md',''),
+      title: meta.data.title
+    })
+  }
 
-	const blogs = files.filter((fn) => fn.endsWith(".md"));
+	// const paths = [{
+	// 	params: { slug: 'cs-undergrad-experience'}
+	// }]
 
-	const paths = blogs.map((blog) => {
-		params: { slug: blog.slug }
-	})
+	const paths = posts.map((post) => ({
+    params: { slug: post.slug },
+  }))
 
-	return { paths, fallback: false }
+	return {paths, fallback: false}
 }
 
 export default PostTemplate;
