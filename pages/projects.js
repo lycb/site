f1881d4cc8b4009f3007d833edcc7833964d73b4
Jenny Ly <lycb@appstate.edu>
2021-01-27 22:05:23 -0500

    populated projects

--- pages/projects.js
+++ pages/projects.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import Link from "next/link";
+import Head from 'next/head';
 import NavBarComponent from '../components/navBar.js';
 
 function Projects() {
   return (
     <>
+      <Head>
+        <title>Projects</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <p className="pages-title">Projects</p>
       <NavBarComponent />
-      <p>coming soon</p>
+      <div class="project-wrapper">
+        <a href="https://ly86.chaulyjenny.com/">
+          <h1 className="posts-title">LY86-64 (2020-<i>present</i>)</h1>
+          <p>A Y86-64 browser-based simulator that focuses on the visualization aspect of the control logic and signals.</p>
+        </a>
+
+        <a href="http://cs.appstate.edu/apphack/">
+          <h1 className="posts-title">AppHack Website (2019-<i>present</i>)</h1>
+          <p>A website for AppHack, a student-run hackathon hosted at Appalachian State University</p>
+        </a>
+
+        <a href="https://github.com/lycb/Together">
+          <h1 className="posts-title">Together; (2020)</h1>
+          <p>A mobile app for students with mental health issues to connect with their community.</p>
+        </a>
+        
+        <a href="https://r2d2.chaulyjenny.com/">
+          <h1 className="posts-title">R2-D2 (2019)</h1>
+          <p>R2-D2 is a command-line tool that generates, stores, and removes your passwords from the comfort of your own terminal</p>
+        </a>
+      </div>
     </>
   )
 }
