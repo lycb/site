ab642d5bcb1e88e0cde66f181a1f7b16643a9376
Jenny Ly <jennyly294@gmail.com>
2021-02-03 01:24:40 -0500

    blog and css

---
+++ content/getting-started.md
@@ -0,0 +1,22 @@
+---
+slug: getting-started
+title: Getting Started With Blogging
+date: Feb 03 2021
+description: "Why I decided to start blogging"
+---
+
+I came across [Shu Ding's blog](https://shud.in/) a couple of months back when I was researching similar projects for my [senior thesis](https://ly86.chaulyjenny.com/home). 
+
+Shu's website has a simple yet elegant feel to it. I like it so much that I decided that I wanted my website to be simple (although I'm not sure if it's as elegant as Shu's).
+
+What I like more than the simplicity was reading Shu's posts (the ones written in English because that was all I could understand). Shu's thoughts on [COVID](https://shud.in/posts/covid-btc-m1) inspired me to start documenting my thoughts and remove unhealthy distractions.
+
+> Last year I started to take notes of my dreams. I started to make creative artworks with code. I started to read poems.
+
+COVID had affected me negatively the past year (as I'm sure it did for many people). I moved back home to be near family for some time, but my study space was in the basement. It was dark, lonely, and cold. I grew sad and couldn't find the strength to do anything productive.
+
+I am happier now and keeping myself motivated. I work on my thesis regularly, study Chinese, and do some self-reflections. 
+
+I also read some blogs now, because people are interesting:
+* [Shu Ding](https://shud.in/)
+* [Kendra Schaefer](http://kendraschaefer.com/)
--- pages/projects.js
+++ pages/projects.js
@@ -14,7 +14,7 @@ function Projects() {
       <NavBarComponent />
       <div class="project-wrapper">
         <div className="tile">
-          <a href="https://ly86.chaulyjenny.com/">
+          <a href="https://ly86.chaulyjenny.com/" className="tile-links">
             <p className="posts-title">LY86-64 (2020-<i>present</i>)</p>
             <img src="/assets/ly86.gif" style={{width: "100%"}}/>
             <p>A Y86-64 browser-based simulator that focuses on the visualization 
@@ -23,7 +23,7 @@ function Projects() {
         </div>
 
         <div className="tile">
-          <a href="http://cs.appstate.edu/apphack/">
+          <a href="http://cs.appstate.edu/apphack/" className="tile-links">
             <p className="posts-title">AppHack Website (2019-<i>present</i>)</p>
               <img src="/assets/apphack.png" style={{width: "100%"}}/>
             <p>A website for AppHack, a student-run hackathon hosted at Appalachian State University.</p>
@@ -31,7 +31,7 @@ function Projects() {
         </div>
 
         <div className="tile">
-           <a href="https://github.com/lycb/Together">
+           <a href="https://github.com/lycb/Together" className="tile-links">
             <p className="posts-title">Together; (2020)</p>
             <img src="/assets/together.gif" style={{width: "50%"}}/>
             <p>A mobile app for students with mental health issues to connect with their community.
@@ -40,7 +40,7 @@ function Projects() {
         </div>
         
         <div className="tile">
-          <a href="https://r2d2.chaulyjenny.com/">
+          <a href="https://r2d2.chaulyjenny.com/" className="tile-links">
             <p className="posts-title">R2-D2 (2019)</p>
             <img src="/assets/r2d2.jpg" style={{width: "100%"}}/>
             <p>R2-D2 is a command-line tool that generates, stores, and removes your passwords from the comfort of your own terminal</p>
