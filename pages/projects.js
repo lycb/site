a0b61f590b22735e04683743e8ac403c3472f6eb
Jenny Ly <jennyly294@gmail.com>
2021-02-02 23:49:42 -0500

    images fix

--- pages/projects.js
+++ pages/projects.js
@@ -16,7 +16,7 @@ function Projects() {
         <div className="tile">
           <a href="https://ly86.chaulyjenny.com/">
             <p className="posts-title">LY86-64 (2020-<i>present</i>)</p>
-            <img src="assets/ly86.gif" style={{width: "100%"}}/>
+            <img src="/assets/ly86.gif" style={{width: "100%"}}/>
             <p>A Y86-64 browser-based simulator that focuses on the visualization 
             aspect of the control logic and signals.</p>
           </a>
@@ -25,7 +25,7 @@ function Projects() {
         <div className="tile">
           <a href="http://cs.appstate.edu/apphack/">
             <p className="posts-title">AppHack Website (2019-<i>present</i>)</p>
-              <img src="assets/apphack.png" style={{width: "100%"}}/>
+              <img src="/assets/apphack.png" style={{width: "100%"}}/>
             <p>A website for AppHack, a student-run hackathon hosted at Appalachian State University.</p>
           </a>
         </div>
@@ -33,7 +33,7 @@ function Projects() {
         <div className="tile">
            <a href="https://github.com/lycb/Together">
             <p className="posts-title">Together; (2020)</p>
-            <img src="assets/together.gif" style={{width: "50%"}}/>
+            <img src="/assets/together.gif" style={{width: "50%"}}/>
             <p>A mobile app for students with mental health issues to connect with their community.
             This project was built overnight at <a href="https://pearlhacks.com/" className="links">PearlHacks</a> in 2020.</p>
           </a>
@@ -42,7 +42,7 @@ function Projects() {
         <div className="tile">
           <a href="https://r2d2.chaulyjenny.com/">
             <p className="posts-title">R2-D2 (2019)</p>
-            <img src="assets/r2d2.jpg" style={{width: "100%"}}/>
+            <img src="/assets/r2d2.jpg" style={{width: "100%"}}/>
             <p>R2-D2 is a command-line tool that generates, stores, and removes your passwords from the comfort of your own terminal</p>
           </a>
         </div>
